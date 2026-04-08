3670966e3f2999ec6952bfe3103da692f827de7e
VANSHIKA <vanshika12.sc@gmail.com>
2026-04-08 10:41:39 +0530

    Delete calculation.py

--- calculation.py
+++
@@ -1,8 +0,0 @@
-# calculation1.py
-from numericcalculation import *
-
-m = 10
-n = 20
-
-print("Sum of", m, "and", n, "is:", addNumbers(m, n))
-print("Product of", m, "and", n, "is:", calculateProduct(m, n))
