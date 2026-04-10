5b506f7f5461d880403c05b1f75ea851c0c03a96
VANSHIKA <vanshika12.sc@gmail.com>
2026-04-10 10:42:31 +0530

    Add files via upload

---
+++ class work/calculation2.py
@@ -0,0 +1,7 @@
+# calculation2.py
+from numericcalculation import addnumbers
+
+m = 10
+n = 20
+print("Sum of", m, "and", n, "is:", addNumbers(m, n))
+print("Product of", m, "and", n, "is:", calculateProduct(m, n))
